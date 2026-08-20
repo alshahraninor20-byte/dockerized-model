49234f91c6d16ca9cb7ae6fc4e64879615fc5ff7
Wasan AlThobaiti <wasanfaiz@outlook.sa>
2026-08-20 13:41:35 +0300

    Implement basic HTTP server to list team members

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
+TEAM = ["Wasan", "norah", "huraa"]
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            message = "Team members: " + ", ".join(TEAM)
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(message.encode())
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+server = HTTPServer(("0.0.0.0", 8000), Server)
+print("Server running on port 8000")
+server.serve_forever()
